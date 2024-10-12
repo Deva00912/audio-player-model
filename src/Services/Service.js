5e909deb62f5a1a83367d5c28798d2e5322eae1c
Devendran <devashiva8778@gmail.com>
2024-10-12 18:24:34 +0530

    debugging error

--- src/Services/Service.js
+++ src/Services/Service.js
@@ -1,4 +1,8 @@
 export const fetchProgramsPagination = async (start = 1, end = 3) => {
+  console.log(
+    "process.env.REACT_APP_baseUrl :>> ",
+    process.env.REACT_APP_baseUrl
+  );
   const response = await fetch(
     `${process.env.REACT_APP_baseUrl}programs?page=${start}&limit=${end}`
   );
